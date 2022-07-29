1567d60a1b98e1699aaf6401df8a0c22d75eac0c
meereh <candimcemk301@gmail.com>
2022-07-29 16:53:29 +0900

    update

--- layout/script.js
+++ layout/script.js
@@ -10,7 +10,12 @@
         return document.getElementById(idname);
     }
 
+    const getClass = (element) => {
+        return document.querySelector(element);
+    }
 
+    const $dropbtn = getClass('.dropbtn');
+    const  $dropbtn1 = getClass('.dropbtn1');
 
     const carousel=() => {
         let x = get("shopimg");
@@ -24,12 +29,42 @@
         setTimeout(carousel, 2000);
     }
 
-
+    const doDropDown = () => {
+        //let $dropdownlist = getId("myDropdown");
+        //console.log($dropdownlist.classList);
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
  
+    
+    const doDropDown1 = () => {
+        //let $dropdownlist = getId("myDropdown");
+        //console.log($dropdownlist);
+        document.getElementById("myDropdown1").classList.toggle("show");
+        
+    }
+
 
     const init = () => {
         carousel();
 
+        window.addEventListener('click',(event) =>{
+            if (!(event.target.matches('.dropbtn') ||event.target.matches('.dropbtn1') )) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+                }
+              }
+            }
+
+            
+          })
+
+       
+        $dropbtn.addEventListener('click', doDropDown);
+        $dropbtn1.addEventListener('click', doDropDown1);
     }
 
     
